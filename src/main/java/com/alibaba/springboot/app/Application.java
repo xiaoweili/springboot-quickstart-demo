3b52af5955d81b3e741089b033b55428eb673d17
zixian.lxw <zixian.lxw@alibaba-inc.com>
2022-11-10 11:09:55 +0800

    init

---
+++ src/main/java/com/alibaba/springboot/app/Application.java
@@ -0,0 +1,16 @@
+package com.alibaba.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zixian.lxw
+ * @date 2022/11/10 10:53
+ * 参考文档：https://www.baeldung.com/spring-boot-start
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/alibaba/springboot/app/entity/User.java
@@ -0,0 +1,11 @@
+package com.alibaba.springboot.app.entity;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zixian.lxw
+ * @date 2022/11/10 11:08
+ */
+@Component
+public class User {
+}
